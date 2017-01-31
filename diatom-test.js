74c0f3ec1e0922b6b8d9515d86f8a44d7f04b5b3
volkovasystems <richeve.bebedor@gmail.com>
2017-02-01 00:36:16 +0800

    Fixed issue on empty parameter in initialize method.
    Fixed build procedure.
    Updated dependencies.

--- diatom-test.js
+++ diatom-test.js
@@ -3,7 +3,14 @@
 const diatom = require( "./diatom.js" );
 
 const Merchant = diatom( "Merchant" );
-Merchant.prototype.initialize = function initialize( hello, world, yeah ){
+Merchant.prototype.initialize = function initialize( ){
+	console.log( arguments );
+};
+
+Merchant( "hello", "world", "yeah" );
+
+const Test = diatom( "Test" );
+Test.prototype.initialize = function initialize( hello ){
 	console.log( arguments );
 };
 
